1028a8dae9f4eabeda1f2b78f23e689f7a29851d
loicdauchy <l.dauchy@codeur.online>
2021-01-06 09:37:30 +0100

    last commit

--- app.js
+++ app.js
@@ -10,22 +10,12 @@ var date = new Date();
 var now = date.getDate();
 var month = date.getMonth();
 var elId = '';
-for(var i = 0; i <= 24; i++){
+for(var i = 0; i < 24; i++){
     elId = cases[i].id;
-    var d = document.getElementById(elId);
-    var b = d.id;
     cases[i].innerHTML = "<h4 id='nb"+elId+"'>"+elId+"</h4><p id='c"+elId+"'>"+gift[i]+"</p>";
-    document.getElementById(elId).addEventListener('click', function(){
-        if(now >= b){ 
-            alert(b)          
-            document.getElementById('nb'+elId).classList.add("hide");
-            document.getElementById('c'+elId).classList.add("show");
-            document.getElementById(elId).classList.add('check');
-            localStorage.setItem('jour'+elId, 'v'+elId);
-            if(localCalenGet === null){
-               localStorage.setItem('', calen.join(""));
-            }}        
-})
+    document.getElementById(elId).onclick = makeOnClickCallback(i);
+    document.getElementById("g"+[i+1]).onclick = makeOnClickCallback2(i);
+    
 var jour = localStorage.getItem('jour'+elId);
 if(jour !== null){
     if(jour.value === 'jour'+elId, 'v'+elId){
@@ -34,6 +24,34 @@ if(jour !== null){
             document.getElementById(elId).classList.add('check');
     }}
 }
+function makeOnClickCallback(i) {  
+    return function() {  
+        elIds = cases[i].id;
+        if(now >= elIds){           
+            document.getElementById('nb'+elIds).classList.add("hide");
+            document.getElementById('c'+elIds).classList.add("show");
+            document.getElementById(elIds).classList.add('check');
+            localStorage.setItem('jour'+elIds, 'v'+elIds);
+            if(localCalenGet === null){
+               localStorage.setItem('', calen.join(""));
+            }
+        }                     
+       return false;
+    };  
+ }
+ function makeOnClickCallback2(i) {  
+    return function() { 
+        document.getElementById('modals').innerHTML = "<div class='modalsContent'>"+gif[i]+"<div id='close"+[i]+"'><i class='close bx bxs-x-circle'></i></div></div>";
+        document.getElementById('modals').classList.add('show');
+        document.getElementById('close'+[i]).addEventListener('click', function(){
+            document.getElementById('modals').classList.remove('show');
+        })                 
+       return false;
+    };  
+ } 
+
+
+
 
 
 
--- tableau.js
+++ tableau.js
@@ -14,16 +14,17 @@ function shuffle(array) {
   }
 
 var calendrier = new Array();
-calendrier[0]= "<div id='1'class='case'></div>";calendrier[1]= "<div id='2'class='case'></div>";calendrier[2]= "<div id='3'class='case'></div>";
-calendrier[3]= "<div id='4'class='case'></div>";calendrier[4]= "<div id='5'class='case'></div>";calendrier[5]= "<div id='6'class='case'></div>";
-calendrier[6]= "<div id='7'class='case'></div>";calendrier[7]= "<div id='8'class='case'></div>";calendrier[8]= "<div id='9'class='case'></div>";
-calendrier[9]= "<div id='10'class='case'></div>";calendrier[10]= "<div id='11'class='case'></div>";calendrier[11]= "<div id='12'class='case'></div>";
-calendrier[12]= "<div id='13'class='case'></div>";calendrier[13]= "<div id='14'class='case'></div>";calendrier[14]= "<div id='15'class='case'></div>";
-calendrier[15]= "<div id='16'class='case'></div>";calendrier[16]= "<div id='17'class='case'></div>";calendrier[17]= "<div id='18'class='case'></div>";
-calendrier[18]= "<div id='19'class='case'></div>";calendrier[19]= "<div id='20'class='case'></div>";calendrier[20]= "<div id='21'class='case'></div>";
-calendrier[21]= "<div id='22'class='case'></div>";calendrier[22]= "<div id='23'class='case'></div>";calendrier[23]= "<div id='24'class='case'></div>"; 
+for(var i = 0; i < 24; i++){
+  calendrier[i]= "<div id='"+[i+1]+"'class='case'></div>";
+}
 
-var gift = [
-    ['coucou'],['coucou2'], ['coucou3'],['coucou4'],['coucou5'],['coucou6'],['coucou7'],['coucou8'],['coucou9'],['coucou10'],['coucou11'],['coucou12'],['coucou13'],['coucou14'],['coucou15'],['coucou16'],['coucou17'],['coucou18'],['coucou19'],['coucou20'],['coucou21'],['coucou22'],['coucou23'],['coucou24']
-];
+var gift = new Array();
+for(var i = 0; i < 24; i++){
+  gift[i]= "<a id='g"+[i+1]+"' href=''><i class='bx bxs-gift'></i></a>";
+}
+
+var gif = new Array();
+for(var i = 0; i < 24; i++){
+  gif[i]= "<img class='imgGift' src='img/image"+[i+1]+".gif'>";
+}
 
